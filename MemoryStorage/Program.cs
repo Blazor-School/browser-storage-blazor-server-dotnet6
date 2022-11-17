9c82176012be1523b9ee16101ae41b7f4a5ed1e4
Red Wei <hevilhuy2@gmail.com>
2022-11-17 20:42:20 +0700

    Added memory storage example.

---
+++ MemoryStorage/Program.cs
@@ -0,0 +1,25 @@
+using MemoryStorage.Utils;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<MemoryStorageUtility>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    _ = app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ MemoryStorage/Utils/MemoryStorageUtility.cs
@@ -0,0 +1,6 @@
+﻿namespace MemoryStorage.Utils;
+
+public class MemoryStorageUtility
+{
+    public Dictionary<string, object> Storage { get; set; } = new();
+}
